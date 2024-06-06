bd1674759fbf7af64d4fe0b0b099bba89edbf2a2
LuisFelipe7215 <luis104.lf@gmail.com>
2024-06-06 11:40:53 -0300

    Cria o atributo isSpellPrepared e o método prepareSpell

--- wizards-and-warriors/src/main/java/Wizard.java
+++ wizards-and-warriors/src/main/java/Wizard.java
@@ -1,6 +1,11 @@
 public class Wizard extends Fighter {
+    boolean isSpellPrepared = false;
     @Override
     public String toString() {
         return "Fighter is a Wizard";
+    }
+
+    public void prepareSpell(){
+        isSpellPrepared = true;
     }
 }
