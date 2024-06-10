f48cbd4df7cfc39ce82d083ce2b2f2086a2cbb35
LuisFelipe7215 <luis104.lf@gmail.com>
2024-06-10 11:54:26 -0300

    Modifica o método calculate

--- calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,22 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+        int finalResult;
+
+        try {
+            switch (operation) {
+                case "" -> throw new IllegalArgumentException("Operation cannot be empty");
+                case null -> throw new IllegalArgumentException("Operation cannot be null");
+                case "+" -> finalResult = operand1 + operand2;
+                case "*" -> finalResult = operand1 * operand2;
+                case "/" -> finalResult = operand1 / operand2;
+                default -> throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
+            }
+
+        } catch (ArithmeticException e) {
+            throw new IllegalOperationException("Division by zero is not allowed", e);
+        }
+
+
+        return String.format("%d %s %d = %d", operand1, operation, operand2, finalResult);
     }
 }
