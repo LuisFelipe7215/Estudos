af9ce2fbc1a29ed8ac6954129d024bb716e9c4a9
LuisFelipe7215 <luis104.lf@gmail.com>
2024-06-17 12:03:28 -0300

    adiciona o construtor

--- logs-logs-logs/src/main/java/LogLevel.java
+++ logs-logs-logs/src/main/java/LogLevel.java
@@ -1,8 +1,14 @@
 public enum LogLevel {
-    TRACE,
-    DEBUG,
-    INFO,
-    WARNING,
-    ERROR,
-    FATAL
+    TRACE("TRC"),
+    DEBUG("DBG"),
+    INFO("INF"),
+    WARNING("WRN"),
+    ERROR("ERR"),
+    FATAL("FTL");
+
+    private final String loglevel;
+
+    LogLevel(String logLevel) {
+        this.loglevel = logLevel;
+    }
 }
