62ada0ae5a2966f3b487d0d69d48690c331f37ae
LuisFelipe7215 <luis104.lf@gmail.com>
2024-06-17 12:02:38 -0300

    Modifica o construtor

--- logs-logs-logs/src/main/java/LogLine.java
+++ logs-logs-logs/src/main/java/LogLine.java
@@ -1,6 +1,17 @@
 public class LogLine {
+    private final String logLine;
 
     public LogLine(String logLine) {
+        char[] chars = logLine.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : chars) {
+            if (Character.isUpperCase(c)){
+                sb.append(c);
+            }
+        }
+
+        this.logLine = sb.toString();
     }
 
     public LogLevel getLogLevel() {
