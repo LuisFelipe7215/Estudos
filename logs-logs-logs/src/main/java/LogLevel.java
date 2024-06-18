1d9c33036559563a22e4e84cac8f1c184d7d4389
LuisFelipe7215 <luis104.lf@gmail.com>
2024-06-18 11:46:32 -0300

    Altera a enum

--- logs-logs-logs/src/main/java/LogLevel.java
+++ logs-logs-logs/src/main/java/LogLevel.java
@@ -1,14 +1,8 @@
 public enum LogLevel {
-    TRACE("TRC"),
-    DEBUG("DBG"),
-    INFO("INF"),
-    WARNING("WRN"),
-    ERROR("ERR"),
-    FATAL("FTL");
-
-    private final String loglevel;
-
-    LogLevel(String logLevel) {
-        this.loglevel = logLevel;
-    }
+    TRACE,
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL;
 }
