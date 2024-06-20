8e3be8d26f9cf1b8f2beaaa4e691eb84c0d8673b
LuisFelipe7215 <luis104.lf@gmail.com>
2024-06-20 11:18:42 -0300

    Adiciona o método

--- wizards-and-warriors-2/src/main/java/GameMaster.java
+++ wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -1,12 +1,13 @@
 public class GameMaster {
 
-    // TODO: define a 'describe' method that returns a description of a Character
 
     public static String describe(Character character){
         return String.format("You're a level %d %s with %d hit points.", character.getLevel(), character.getCharacterClass(), character.getHitPoints());
     }
 
-    // TODO: define a 'describe' method that returns a description of a Destination
+    public static String describe(Destination destination){
+        return String.format("You've arrived at %s, which has %d inhabitants.", destination.getName(), destination.getInhabitants());
+    }
 
     // TODO: define a 'describe' method that returns a description of a TravelMethod
 
